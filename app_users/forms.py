037712b9d40aa9122ec42d56f556b05b0fd297e7
Ulug'bek Umaraliyev <thedevu101@gmail.com>
2024-10-30 05:50:58 +0500

    Update login and registration pages, Add error, success and warning messages for login and registration pages

--- app_users/forms.py
+++ app_users/forms.py
@@ -1,23 +1,35 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 User = get_user_model()
 
 
-class UserRegistrationForm(forms.ModelForm):
-    password1 = forms.CharField(
-        max_length=100, widget=forms.PasswordInput(attrs={'type': 'password'}))
-    password2 = forms.CharField(
-        max_length=100, widget=forms.PasswordInput(attrs={'type': 'password'}))
-
+class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['email', 'first_name', 'last_name', 'password1', 'password2']
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
         for _, field in self.fields.items():
             field.widget.attrs.update({
                 'class': 'outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
             })
+
+            if _ == 'email':
+                field.widget.attrs.update({
+                    'autofocus': 'true',
+                })
+
+
+class UserAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for _, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            })
+
--- app_users/views.py
+++ app_users/views.py
@@ -4,8 +4,9 @@ from django.contrib.auth.views import LoginView
 from django.views.generic import CreateView
 from django.shortcuts import redirect
 from django.contrib.auth import logout, get_user_model
+from django.contrib import messages
 
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserAuthenticationForm
 
 User = get_user_model()
 
@@ -16,8 +17,26 @@ class UserLoginView(LoginView):
     extra_context = {
         'title': 'Login'
     }
+    form_class = UserAuthenticationForm
 
+    def get_success_url(self) -> str:
+        messages.success(self.request, f'Welcome, {self.request.user.fullname} 👋')
+        return super().get_success_url()
 
+    def form_invalid(self, form):
+        email = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user_exists = get_user_model().objects.filter(email=email).exists()
+
+        if not user_exists:
+            messages.error(self.request, 'User was not found')
+        elif user_exists and ('@' in email and not get_user_model().objects.get(email=email).check_password(password)):
+            messages.error(self.request, 'Email or Password is incorrect')
+        else:
+            messages.error(self.request, 'Form filled incorrectly')
+
+        return super().form_invalid(form)
+    
 
 def user_registration(request):
     form = UserRegistrationForm(request.POST or None)
@@ -25,17 +44,15 @@ def user_registration(request):
     if request.method == 'POST':
         if form.is_valid():
             password1, password2 = request.POST.get('password1'), request.POST.get('password2')
+            email = request.POST.get('email')
 
             if (password1 and password2) and len(password2) >= 8:
                 user = form.save(commit=False)
+                user.username = email[:email.index('@')]
                 user.set_password(password2)
                 user.save()
-                # success message
+                messages.success(request, f'You have been successfully registered, now you can log in here 😀')
                 return redirect('login')
-            else:
-                ...
-        else:
-            ...
 
     context = {
         'form': form,
